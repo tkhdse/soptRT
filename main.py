bad57546a3907f208553007e23a3593ee174ac7f
Tanish <tkhadse2@illinois.edu>
2026-01-04 01:54:22 -0600

    neural network definition

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from torch.fx import symbolic_trace, GraphModule
 
+class Net(nn.Module):
+    def __init__(self, input_size, num_classes):
+        super().__init__()
+        self.fc1 = nn.Linear(input_size, 16)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(16, num_classes)
 
-def my_func(x):
-    return torch.relu(x).neg()
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        return self.fc2(x)
 
-traced = symbolic_trace(my_func)
+
+
+net = Net(8, 2)
+traced = symbolic_trace(net)
 print(traced.graph)
