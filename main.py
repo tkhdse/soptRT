4b992bee0eb8f3fd9b336b4c498b1572bbf62725
Tanish <tkhadse2@illinois.edu>
2026-01-04 03:06:23 -0600

    torch.export

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.fx import symbolic_trace, GraphModule
 
 class Net(nn.Module):
-    def __init__(self, input_size, num_classes):
+    def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(input_size, 16)
+        self.fc1 = nn.Linear(8, 16)
         self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(16, num_classes)
+        self.fc2 = nn.Linear(16, 4)
 
     def forward(self, x):
         x = self.relu(self.fc1(x))
         return self.fc2(x)
 
 
+net = Net()
+# traced = symbolic_trace(net)
+# print(traced.graph)
 
-net = Net(8, 2)
-traced = symbolic_trace(net)
-print(traced.graph)
+module = torch.export.export(net, (torch.randn(100, 8), ))
+print(module.graph)
