48a499efc6a73ca8d1e29837f097a862cf16ed18
Tanish <tkhadse2@illinois.edu>
2026-01-05 17:16:52 -0600

    graph parser setup

--- python/sopt/__init__.py
+++ python/sopt/__init__.py
@@ -4,9 +4,8 @@ import soptRT
 
 def compile(model: nn.Module):
     module = torch.export.export(model(), (torch.randn(100, 8), ))
-    print(module.graph)
-    print('\n')
-    print(module.run_decompositions().graph)
 
-    print(soptRT.sum_as_string(20,5))
+    rgraph = soptRT.parse_graph(str(module.run_decompositions().graph))
+    print(rgraph)
+
 
--- src/lib.rs
+++ src/lib.rs
@@ -10,4 +10,10 @@ mod soptRT {
     fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
         Ok((a - b).to_string())
     }
+
+
+    #[pyfunction]
+    fn parse_graph(graph: String) -> PyResult<String> {
+        Ok(graph)
+    }
 }
