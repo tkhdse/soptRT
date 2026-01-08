17dd3040441be93259d3e5de8ef53a83ca28c9c4
Tanish <tkhadse2@illinois.edu>
2026-01-08 02:48:03 -0600

    self node

--- src/dialects/soptfuse.rs
+++ src/dialects/soptfuse.rs
@@ -1,1 +1,5 @@
 // Dialect representting soptfx post-fusion
+
+// Rust conversion pass would look for "soptRT.aten_add" (Opaque) and replace with a Registered Op "arith.addf"
+
+// Can we make this compiler run faster by immediately replacing all aten_add's from the FX graph with "arith.addf"? 
