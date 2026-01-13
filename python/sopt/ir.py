9123146c53f027ad437b0e6bad13b062bb359278
Tanish <tkhadse2@illinois.edu>
2026-01-12 18:11:51 -0600

    updated Python node payload

--- python/sopt/ir.py
+++ python/sopt/ir.py
@@ -1,24 +1,48 @@
 import torch
 from dataclasses import dataclass
+from typing import List, Optional
 
 @dataclass
 class Node:
     name: str
     op_name: str
     target: str
-    args: list
+    args: List[str]
+
+    # optional fields
+    shape: Optional[List[int]] = None
+    dtype: Optional[str] = None
+    index: Optional[int] = None
 
 def serialize_fx_to_json(graph: torch.fx.GraphModule):
     print(graph)
     
     nodes_data = []
 
-    for node in graph.nodes:
-        nodes_data.append(Node(
-            node.name,
-            node.op,
-            str(node.target),
-            [str(arg) for arg in node.args]
-        ))
+    for index,node in enumerate(graph.nodes):
+        val = node.meta.get('val')
+        if node.op != "output":
+            shape = list(val.shape)
+            dtype = str(val.dtype)
+
+            nodes_data.append(Node(
+                node.name,
+                node.op,
+                str(node.target),
+                [str(arg) for arg in node.args]
+                shape,
+                dtype,
+                index
+            ))
+        else:
+            nodes_data.append(Node(
+                node.name,
+                node.op,
+                str(node.target),
+                [str(arg) for arg in node.args]
+                None,
+                None,
+                index
+            ))
 
     return nodes_data
